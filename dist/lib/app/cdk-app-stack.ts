8847826b9c305bd65ca1e7c6102b8e66fe337b40
zabideen <zainulabideen1258@gmail.com>
2024-02-12 22:44:48 +0100

    added s3 and cloudfront distribution

--- dist/lib/app/cdk-app-stack.ts
+++ dist/lib/app/cdk-app-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 import { LambdaApiGateway } from './constructs/lambda-apigw';
+import { CloudFrontS3 } from './constructs/cloudfront-s3';
 
 export class HelloWorldApp extends cdk.Stack {
 
@@ -9,5 +10,6 @@ export class HelloWorldApp extends cdk.Stack {
     super(scope, id, props);
 
     new LambdaApiGateway(this, 'LambdaApiGateway');
+    new CloudFrontS3(this, 'CloudFrontS3');
   }
 }
